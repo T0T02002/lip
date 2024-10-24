f1d2dc08d6d356f0831a4c3550279d94f6380603
T0T02002 <salvamadeddu2002@tiscali.it>
2024-10-24 10:42:10 +0200

    aggiornato exec .sh

--- toylexer/COMEHOFATTO.sh
+++ toylexer/COMEHOFATTO.sh
@@ -3,3 +3,9 @@ cd toylexer
 dune build 
 dune utop
 # frequency 3 [ID("x"); ASSIGN; ID("y"); SEQ; ID("x"); ASSIGN; ID("x"); PLUS; CONST("1")];;
+
+
+# per quanto riguarda dune exec
+cd toylexer
+dune exec toylexer freq <n>  #n param
+#x+x=y           
